a0a7f2d59b01d30186a89e4ce85ad845b411efd2
Azhar457 <azharsss457@gmail.com>
2025-10-27 19:25:36 +0700

    Upload

--- Pertemuan6/tugas1.java
+++ Pertemuan6/tugas1.java
@@ -3,40 +3,38 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
-
 public class tugas1 {
-
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-           @Override
            public void run() {
-
             JFrame frame = new JFrame();
             frame.setTitle("Tugas 1 Komponen Gui Dasar");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(400, 200);
-            
-            JButton mulai = new JButton();
-            mulai.setText("😃");
-
-            JLabel label = new JLabel();
-            label.setText("Label");
-            
-            mulai.addActionListener(e -> {
-                mulai.setText("Tombol Di Klik");
-                label.setText("Tombol Di Klik");
+            JButton center = new JButton("Tombol Center");
+            JButton west = new JButton("Tombol West");
+            JButton south = new JButton("Tombol South");
+            JButton east = new JButton("Tombol East");
+            JLabel label = new JLabel("Label");
+            center.addActionListener(e -> {
+                label.setText("Tombol Center di Klik");
             });
-
+            
+            west.addActionListener(e -> {
+                label.setText("Tombol West di Klik");
+            });
+            south.addActionListener(e -> {
+                label.setText("Tombol South di Klik");
+            });
+            east.addActionListener(e -> {
+                label.setText("Tombol East di Klik");
+            });
             frame.setLayout(new BorderLayout());
             frame.add(label,BorderLayout.NORTH);
-
-            frame.add(new JButton("Tombol 1"),BorderLayout.NORTH);
-            frame.add(new JButton("Tombol 2"),BorderLayout.WEST);
-            frame.add(new JButton("Tombol 3"),BorderLayout.EAST);
-            frame.add(new JButton("Tombol 4"),BorderLayout.SOUTH);
-            frame.add(mulai,BorderLayout.CENTER);
-            
-
+            frame.add(west,BorderLayout.WEST);
+            frame.add(east,BorderLayout.EAST);
+            frame.add(south,BorderLayout.SOUTH);
+            frame.add(center,BorderLayout.CENTER);
             frame.setVisible(true);
         } 
     });
---
+++ README.md
@@ -0,0 +1,75 @@
+# 📚 Praktikum Pemograman II - IF21W0508
+
+> "Tulis *quote* favoritmu tentang belajar atau deskripsi singkat satu kalimat yang menarik soal mata kuliah ini di sini."
+
+**Status:** 🚧 Sedang Berlangsung / ✅ Selesai / ⏳ Ditunda
+
+---
+
+## 👤 Data Diri
+
+| Atribut | Keterangan |
+| :--- | :--- |
+| **Nama** | Azhar Muttaqien |
+| **NIM** | 233040126 |
+| **Kelas** |  A |
+| **Tahun Ajaran** | 2025/2026 - Ganjil |
+
+---
+
+## 👨‍🏫 Informasi Mata Kuliah
+
+* **Dosen Pengampu:** Wanda Gusdya Purnama ST., MT.
+* **Email Dosen:** [Email Dosen]
+* **Deskripsi Singkat:**
+    Mata Kuliah Terkait Pemograman Java dengan GUI berfokus Pada Swing
+
+## 🗺️ Peta Pembelajaran (Syllabus)
+
+Ini adalah daftar materi yang akan dibahas, kamu bisa tandai (checklist) materi yang sudah kamu kuasai.
+
+* [ ] **Minggu 1:** Pengenalan [Topik Minggu 1]
+* [ ] **Minggu 2:** [Topik Minggu 2]
+* [ ] **Minggu 3:** [Topik Minggu 3]
+* [ ] **Minggu 4:** [Topik Minggu 4]
+* [ ] **Minggu 5:** [Topik Minggu 5]
+* [ ] **Minggu 6:** [Topik Minggu 6]
+* [ ] **Minggu 7:** [Topik Minggu 7]
+* [ ] **Minggu 8: UTS (Ujian Tengah Semester)** 📝
+* [ ] **Minggu 9:** [Topik Minggu 9]
+* [ ] **Minggu 10:** [Topik Minggu 10]
+* [ ] **Minggu 11:** [Topik Minggu 11]
+* [ ] **Minggu 12:** [Topik Minggu 12]
+* [ ] **Minggu 13:** [Topik Minggu 13]
+* [ ] **Minggu 14:** [Topik Minggu 14]
+* [ ] **Minggu 15:** Review Materi
+* [ ] **Minggu 16: UAS (Ujian Akhir Semester)** 🏆
+
+## 🎯 Tugas & Proyek
+
+Daftar tugas, kuis, dan proyek yang harus dikerjakan.
+
+| Jenis | Topik/Deskripsi | Status | Deadline |
+| :--- | :--- | :---: | :---: |
+| Kuis 1 | [Topik Kuis] | [ ✅ / ❌ ] | [Tgl] |
+| Tugas 1 | [Deskripsi Tugas] | [ ✅ / ❌ ] | [Tgl] |
+| Proyek UTS | [Deskripsi Proyek] | [ ✅ / ❌ ] | [Tgl] |
+| Proyek UAS | [Deskripsi Proyek] | [ ✅ / ❌ ] | [Tgl] |
+
+## 📚 Sumber Daya (Resources)
+
+Kumpulan tautan penting, *software*, atau buku yang digunakan.
+
+* **Buku Teks Utama:** 
+* **Slide Perkuliahan:** https://aksesif.edu-pas.id/course/view.php?id=48
+* **Contoh Kode:** 
+* **Software Wajib:** vscode, Netbeans
+
+## 📝 Catatan Tambahan
+
+Tidak Ada
+
+---
+<p align="center">
+  Dibuat dengan ❤️ oleh Azhar Muttaqien
+</p>
